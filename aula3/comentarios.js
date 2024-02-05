2da4c1651e3aa9806f866622335d1478ed72e890
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-05 13:23:59 -0300

    add > aula3 /comentarios

---
+++ aula3/comentarios.js
@@ -0,0 +1,9 @@
+//e escrever um comentário
+console.log('olá', 'mundo'); //aqui tem outrro comentário
+
+console.log('linha 1')
+/*
+console.log('linha 2')
+console.log('linha 3')
+*/
+console.log('linha 4')
