3ba5427e02f1d36be73879c9a105909c8c32b7b8
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-06 14:05:11 -0300

    operadores de comparação

---
+++ aula4/logica.js
@@ -0,0 +1,20 @@
+/*
+    operadores de comparação
+
+    >maior que
+    >=maior que ou igual a
+
+    <menor que
+    <=menor que ou igual a 
+
+    ===igualdade estrita (valor e tipo)
+
+    !== diferente estrito (valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = '11';
+
+const espressao = num1 !== num2;
+console.log(espressao);
+
