0c2711ce123501bda5b351f15063759863cf35da
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-07 16:29:08 -0300

    add > aula7

---
+++ aula7/index.js
@@ -0,0 +1,34 @@
+//não podemos criar variaveis com palavras reservadas
+//variaveis precisam ter nomes significativos
+
+/*
+let NomeCliente = 'vitor';
+console.log(NomeCliente);
+*/
+
+//nao pode começar o nome de uma variavel com um número
+//let 1nome;
+
+
+//nao podem conter espaços ou traços
+
+//utilizamos camelCase
+//let nomeCompletoDoCliente = 'jose da silva perreira';
+//console.log(nomeCompletoDoCliente)
+
+
+//Case-sensitive 
+//let nomeCliente = 'luiz'
+//let nomecliente = 'gabriel'
+//console.log(nomeCliente, nomecliente)
+
+
+//nao podemos redeclarar variaveis com let
+//NAO UTILIZE VAR E SIM LET!
+
+let nome = 'gabriel';
+console.log(nome,'nasceu em 1984');
+console.log('em 2000', nome, 'conheceu maria.');
+console.log(nome, 'casou-se com maria em 2012');
+console.log('maria teve 1 filho com', nome, 'em 2015');
+console.log('o filho de', nome, 'se chama eduardo');
