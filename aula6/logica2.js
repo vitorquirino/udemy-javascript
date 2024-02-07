0b66b665f31c05e30a5ebdc33f722c3381c62f8e
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-07 13:26:40 -0300

    operadores lógicos

---
+++ aula6/logica2.js
@@ -0,0 +1,19 @@
+/*operadores lógicos
+
+&& -> AND -> E -> todas as expressões precisam ser verdadeiras para retornar true 
+
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+//const expressaoAnd = true && true && true && true;
+//console.log(expressaoAnd);
+
+//const expressaoOr = false || false  || true || false ;
+//console.log(expressaoOr);
+
+//const usuario ='luiz';
+//const senha = '23456';
+//const vailogar = usuario === 'luiz' &&  senha === '23456';
+//console.log(vailogar)
+
+console.log(!false)
