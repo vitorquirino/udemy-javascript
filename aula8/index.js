345c7ac43a7b7cdbb5bc36ed002968266f125191
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-09 00:08:55 -0300

    const

---
+++ aula8/index.js
@@ -0,0 +1,7 @@
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+console.log(resultado)
+console.log(resultadoDuplicado)
+console.log(typeof primeiroNumero)
