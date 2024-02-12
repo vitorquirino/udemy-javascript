21592c4e3d3450abb2efe40675b99fcc15f89673
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-12 12:22:57 -0300

    Avaliação de Curto-circuito!

---
+++ aula9/logica.js
@@ -0,0 +1,15 @@
+/*
+const CorUsuario = 'vermelho';
+const CorPadrao = CorUsuario || 'yellow';
+
+console.log(CorPadrao)
+*/
+
+
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e )
