b8545c566d185bf61ef4a12c4cf051446a62cafb
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-19 12:24:55 -0300

    Tipos de dados primitivos

---
+++ aula14/index.js
@@ -0,0 +1,17 @@
+// strign , number , undefined , null, bolean, symbol
+const nome1 = 'luiz'; //string
+const nome2 = "bruno"; //string
+const nome3 = `felipe`; //string
+const num1 = 10; //number
+const num2 = 10.62; //number
+let nomedoAluno; // undefined - não apont para local nenhum na memória
+const sobrenomeAluno = null; //nulo - não apont para local nenhum na memória
+const aprovado = true; // boolean = true , false (lógico)
+
+console.log(typeof aprovado , aprovado)
+let a = 2;
+let b = a;
+console.log(a , b)
+
+a = 3;
+console.log(a, b)
