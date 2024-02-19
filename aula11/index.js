18a34d293149450b66f7291a3fbe78e212008d13
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-19 10:06:43 -0300

    if, else if e else (1)

--- aula11/index.js
+++ aula11/index.js
@@ -2,4 +2,44 @@
 if e else
 */
 
-console.log('ola geo');
+/*
+if pode ser usado sozinho
+*/
+
+/*
+sempre que utilizo a palavra else , preciso de um if antes 
+eu posso ter vários else e ifs na checagem
+só posso ter um else na checagem
+podemos usar consições sem else if , utilizando apenas if e else
+*/
+
+
+/*
+entre 0 e 11 <= - bom dia
+entre 12 e 17 - boa tarde
+entre 18 e 23 - boa noite
+*/
+const hora = 90;
+if(hora >= 0 && hora <= 11){
+    console.log('BOM DIA');
+}
+else if(hora >= 12 && hora <= 17){
+    console.log('BOA TARDE');
+}
+else if (hora >= 18 && hora <= 23  ) {
+    console.log('BOA NOITE');
+}
+else{
+    console.log('ESSE HORARIO NAO EXISTE , TENHA UM BOM DIA')
+}
+
+
+
+
+
+
+/*
+else{
+    console.log('BOA TARDE');
+}
+*/
