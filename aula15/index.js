7aa4ca75135830adf411598a3bd1b89840d811c5
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-20 12:08:32 -0300

    add

---
+++ aula15/index.js
@@ -0,0 +1,19 @@
+/*
+    () vai ser executado primeiro
+    **
+    * / %
+    + -
+    incremento = ++
+    decremento = --
+*/
+
+/*
+operadores de atribuição
+*/ 
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);
+console.log(typeof num2);
+
+
+
