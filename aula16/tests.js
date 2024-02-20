a40a3b2d6483fd74ac2872d415d7d2fb4e4780fb
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-20 16:27:36 -0300

    adds

---
+++ aula16/tests.js
@@ -0,0 +1,6 @@
+let n1 = parseInt(prompt('Digite um numero.'));
+let n2 = parseInt(prompt('Digite outro numero.'));
+let resultadoDaConta =n1 + n2;
+
+window.alert(`O resultado da conta foi de:  ${resultadoDaConta} certinho!10
+`)
