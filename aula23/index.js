72de9f81efc42bc4988457884aedc1888836fe09
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-02-27 16:40:18 -0300

    arrays!

---
+++ aula23/index.js
@@ -0,0 +1,42 @@
+//            012345678910
+//const nome = 'luiz otavio'
+//console.log(nome[10])
+
+
+//                0         1        2
+const alunos = ['luiz',  'maria',  'joão'];
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+//delete alunos[1]
+
+//const removida = alunos.shift()
+//console.log(`isso foi removido de alunos  : ${removida}`)
+
+
+
+//alunos.unshift('Luiza')
+//alunos.unshift('fabiozera')
+
+
+//alunos.push('ótavio');
+//alunos.push('fabio');
+
+
+
+//alunos[alunos.length] = 'luizaa';
+//alunos[alunos.length] = 'febio';
+//alunos[alunos.length] = 'luana';
+
+
+
+
+//alunos[0] = 'eduardo';
+//alunos[3] = 'luiza';
+
+
+/*
+console.log(alunos)
+console.log(alunos[0])
+console.log(alunos[2]) 
+*/
