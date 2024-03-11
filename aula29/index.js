1d5ce41a7bab521e1ee7f76337a4b6a9ee96efdc
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-03-11 14:25:47 -0300

    add - operador ternário

---
+++ aula29/index.js
@@ -0,0 +1,12 @@
+// ? :
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario);
+
+
+
+//if(pontuacaoUsuario >= 1000 ){
+    //console.log('Usuário VIP');
+//}else{
+    //console.log('Usuário normal')
+//}
