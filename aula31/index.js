f13d66a7dc7f560b0abe692eb8ddcd4a7ace488c
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-03-28 23:22:35 -0300

    Create index.js

---
+++ aula31/index.js
@@ -0,0 +1,6 @@
+//const tresHoras = 60 * 60 * 3 *1000;
+//const umDia = 60 * 60 * 24 * 1000;
+//const data = new Date(0 + tresHoras + umDia); // 01/01/1970 timestamp unix
+
+const data = new Date(2019,3,20, 15,14,47,999);
+console.log(data.toString());
