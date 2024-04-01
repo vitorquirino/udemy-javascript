0af859c5a8f685d75292c022904f08f2f848d29a
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-04-01 11:11:38 -0300

    Date

--- aula31/index.js
+++ aula31/index.js
@@ -1,6 +1,24 @@
 //const tresHoras = 60 * 60 * 3 *1000;
 //const umDia = 60 * 60 * 24 * 1000;
 //const data = new Date(0 + tresHoras + umDia); // 01/01/1970 timestamp unix
+//const data = new Date(2024, 4); //a, m, d , h, M , s, ms
 
-const data = new Date(2019,3,20, 15,14,47,999);
-console.log(data.toString());
+
+function zeroAEsquerda (numero){
+    return numero >= 10 ? numero : `0${numero}`;
+}
+
+function formataDate (data){
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora =zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+const data = new Date();
+const dataBrasil = formataDate(data);
+console.log(dataBrasil)
+
