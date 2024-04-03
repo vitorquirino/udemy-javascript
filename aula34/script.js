e832a7ecde9717047b764e9a5bd9c162c2293ae4
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-04-03 13:20:22 -0300

    mais diferenças entre var e let/const

---
+++ aula34/script.js
@@ -0,0 +1,37 @@
+//let tem escopo de bloco {`...bloco}
+//var só tem escopo de função
+const vardadeira = true;
+/*
+let nome = 'luiz'; //criando
+var nome2 = 'vitor'; //criando
+
+
+if(vardadeira) {
+    let nome = 'otavio' //criando
+    var nome2 = 'rogero'; //redeclarando
+    
+    if(vardadeira){
+        var nome2 = 'ronaldo' //redeclarando
+        let nome = 'outra coisa'
+    }
+}
+console.log(nome,nome2)
+*/
+
+
+/*
+var sobrenome = 'quirino'
+function falaOi(){
+    if(vardadeira){
+        let nome = 'luiz'
+        var sobrenome = 'miranda'
+        
+    }
+    console.log(sobrenome)
+}
+falaOi();
+*/
+
+console.log(sobrenome)
+let sobrenome = 'miranda'
+
