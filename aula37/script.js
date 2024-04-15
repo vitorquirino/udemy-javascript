2f26af2fd6d2b73ed9e2cfb621f2d6a22028b871
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-04-15 12:08:12 -0300

    add

---
+++ aula37/script.js
@@ -0,0 +1,15 @@
+/*
+console.log('linha 0');
+console.log('linha 1');
+console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4');
+console.log('linha 5');
+*/
+                //0      1      2
+const frutas = ['maça','banana','uvas', 'uva', 'morango'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`indice ${i}`,frutas[i]);
+    
+}
