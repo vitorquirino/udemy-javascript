20802a5064acd2ad19324d0e0a9d5e8f3938ef77
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-05-06 15:43:38 -0300

    for in

---
+++ ex40/script.js
@@ -0,0 +1,21 @@
+
+const p ={
+    nome: 'joao',
+    sobrenome: 'silva',
+    idade:30
+};
+
+for(let chave in p){
+    console.log(chave, p[chave])
+}
+
+//for in -> le os indices ou chaves do objeto 
+
+//const frutas = ['pera','maça','uva'];
+//for(let indice in frutas){
+ //   console.log(frutas[indice])
+//}
+
+//for (let i = 0; i < frutas.length; i++){
+//    console.log(frutas[i])
+//}
