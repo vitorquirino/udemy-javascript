21ee3e527394082844aac2bc2f1ecb696864397f
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-05-13 18:46:36 -0300

    add!

---
+++ ex42/script2.js
@@ -0,0 +1,10 @@
+const paragrafos = document.querySelectorAll('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody)
+
+for (let p of ps ){
+    //console.log(p)
+}
