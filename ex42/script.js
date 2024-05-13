f7c84f0a22eed80a13eac50c81190ef4fa28cc28
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-05-13 19:17:32 -0300

    add

--- ex42/script.js
+++ ex42/script.js
@@ -1,10 +1,11 @@
-const paragrafos = document.querySelectorAll('.paragrafos');
+const paragrafos = document.querySelector('.paragrafos');
 const ps = paragrafos.querySelectorAll('p');
 
 const estilosBody = getComputedStyle(document.body);
 const backgroundColorBody = estilosBody.backgroundColor;
 console.log(backgroundColorBody)
 
-for (let p of ps ){
-    //console.log(p)
+for (let p of ps){
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#FFFFFF'
 }
