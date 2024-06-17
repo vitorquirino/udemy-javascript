ee261020039983f7f2343f7996aedda314e6f9d1
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-06-17 15:30:03 -0300

    relembrando!

--- ex47/script.js
+++ ex47/script.js
@@ -1,5 +1,8 @@
-let num1 = parseFloat(prompt('Digte um numero'));
-let num2 = parseFloat(prompt('Digite outro numero'));
-
-const result = (num1 + num2);
-alert('seu resultado é ' + result);
+let n1 = parseFloat(prompt('digite um numero'));
+let n2 = parseFloat(prompt('digite outro numero'));
+const sub =(n1 - n2);
+const mult =((n1 * n2));
+const div =(n1 / n2);
+const soma =(n1 + n2);
+alert('a subtraçao é ' + sub + ' a multiplicação é ' + mult + ' a divisão é ' + div + ' A soma é ' + soma);
+console.log(soma)
