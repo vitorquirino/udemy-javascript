d40386eeabe8f96e4af3040f97d0fa4ada94bbe6
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-06-17 15:59:22 -0300

    exercício!

--- ex47/script.js
+++ ex47/script.js
@@ -1,8 +1,11 @@
-let n1 = parseFloat(prompt('digite um numero'));
-let n2 = parseFloat(prompt('digite outro numero'));
-const sub =(n1 - n2);
-const mult =((n1 * n2));
-const div =(n1 / n2);
-const soma =(n1 + n2);
-alert('a subtraçao é ' + sub + ' a multiplicação é ' + mult + ' a divisão é ' + div + ' A soma é ' + soma);
-console.log(soma)
+function fizzBuzz(numero){
+    if (typeof numero !== 'number' ) return numero
+    if (numero % 3 === 0 && numero % 5 === 0) return  'FizzBuzz'
+    if (numero % 3 === 0) return  'Fizz'
+    if (numero % 5 === 0) return  'Buzz'
+    return numero
+}
+
+for(let i = 0; i<= 100; i++){
+    console.log(i,fizzBuzz(i))
+}
