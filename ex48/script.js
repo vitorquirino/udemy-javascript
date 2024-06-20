fabe3c5637f0496d22ce2b2dad42e88f913062ef
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-06-20 17:05:54 -0300

    Exercício de revisão!

---
+++ ex48/script.js
@@ -0,0 +1,8 @@
+let n1 = parseInt(prompt('Digite um numero'));
+let n2 = parseInt(prompt('Digite outro numero'));
+
+const resultadoModulo = (n1 % n2)
+const resultadoElevado = (n1 ** n2)
+
+alert('o resto da divisão de ' + n1 + ' por '+ n2 + ' é: ' + resultadoModulo);
+alert('o numero ' + n1 + ' eleveado a ' + n2 + ' é: ' + resultadoElevado);
