00205673230784b55f00b5c704a389c69c09d120
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-06-24 15:23:06 -0300

    relembrando!

--- ex48/script.js
+++ ex48/script.js
@@ -1,8 +1,5 @@
-let n1 = parseInt(prompt('Digite um numero'));
-let n2 = parseInt(prompt('Digite outro numero'));
+const pNumero = parseFloat(prompt('digite um numero'));
+const sNumero = parseFloat(prompt('digite outro numero'));
 
-const resultadoModulo = (n1 % n2)
-const resultadoElevado = (n1 ** n2)
-
-alert('o resto da divisão de ' + n1 + ' por '+ n2 + ' é: ' + resultadoModulo);
-alert('o numero ' + n1 + ' eleveado a ' + n2 + ' é: ' + resultadoElevado);
+const media = (pNumero + sNumero) / 2;
+alert(media > 5);
