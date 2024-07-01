efb5e73e1c5279c5b473f26ed5068fd380a592c1
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-07-01 13:19:35 -0300

    relembrando!

--- ex48/script.js
+++ ex48/script.js
@@ -1,9 +1,25 @@
-var n1 = parseFloat(prompt('digite um numero')); 
-var n2 = parseFloat(prompt('digite outro numero'));
-const result = (n1 + n2) / 2;
+var jogador1 = parseFloat(prompt('digite um numero'));
+var jogador2 = parseFloat(prompt('digite outro numero'));
 
-if(result > 5){
-    alert("sua media foi " + result + " prabens voce foi aprovado");
-} else{
-    alert("sua media foi " + result + " reprovado, estude mais");
+if(jogador1 === jogador2){
+    alert('voces empataram')
+}else{
+    var sort = parseInt( Math.random() * 2);
+
+    if(sort === 0) {
+        if(jogador1 < jogador2){
+            alert('ganhador foi o jogador1');
+        }else{
+            alert('ganhador foi o jogador 2');
+        }
+    }
+    else{
+        if(jogador1 > jogador2){
+            alert('ganhador foi o jogador 2');
+        }else{
+            alert('ganhador foi o jogador1');
+        }
+    }
 }
+
+
