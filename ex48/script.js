df1569b456b783a192a2cb4fac224693f46fd32e
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-07-04 15:50:56 -0300

    exercício com operadores lógicos!

--- ex48/script.js
+++ ex48/script.js
@@ -1,34 +1,17 @@
 // ||
 // && tudo tem que ser true
 // !
+var player1 = parseFloat(prompt('numero do player 1'));
+var player2 = parseFloat(prompt('numero do player 2'));
 
+const numeroSorteado = parseInt(Math.random() * 2);
+alert('numero sorteado : ' +numeroSorteado);
 
+if( (numeroSorteado === 0 && player1 < player2) || (numeroSorteado === 1 && player1 > player2)){
+    alert('ganhador player1')
+}else{
+    alert('ganhador player2')
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var num1 = parseFloat(prompt('digite um  numero'));
-// var num2 = parseFloat(prompt('digite outro numero')); 
-// var media = (num1 + num2) / 2;
-
-// if(media > 5 && num1 > 0  && n2 > 0) {
-    // alert('parabens voce foi aprovado');
-// }
-// else{
-    // alert('que pena ,voce foi reprovado');
-// }
 
 
