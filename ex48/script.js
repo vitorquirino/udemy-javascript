1d92c214e738fc4a8358f8e6e0d9d2bffff6ebc2
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-07-04 14:26:03 -0300

    exercício basico

--- ex48/script.js
+++ ex48/script.js
@@ -1,15 +1,34 @@
 // ||
 // && tudo tem que ser true
 // !
-var num1 = parseFloat(prompt('digite um  numero'));
-var num2 = parseFloat(prompt('digite outro numero')); 
-var media = (num1 + num2) / 2;
 
-if(media > 5 && num1 > 0  && n2 > 0) {
-    alert('parabens voce foi aprovado');
-}
-else{
-    alert('que pena ,voce foi reprovado');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = parseFloat(prompt('digite um  numero'));
+// var num2 = parseFloat(prompt('digite outro numero')); 
+// var media = (num1 + num2) / 2;
+
+// if(media > 5 && num1 > 0  && n2 > 0) {
+    // alert('parabens voce foi aprovado');
+// }
+// else{
+    // alert('que pena ,voce foi reprovado');
+// }
 
 
