d6bb2ea53229888362be4a06784c257e84cdd347
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-07-08 11:33:35 -0300

    Update script.js

--- ex48/script.js
+++ ex48/script.js
@@ -1,17 +1,7 @@
-// ||
-// && tudo tem que ser true
-// !
-var player1 = parseFloat(prompt('numero do player 1'));
-var player2 = parseFloat(prompt('numero do player 2'));
+// switch
+//case
 
-const numeroSorteado = parseInt(Math.random() * 2);
-alert('numero sorteado : ' +numeroSorteado);
 
-if( (numeroSorteado === 0 && player1 < player2) || (numeroSorteado === 1 && player1 > player2)){
-    alert('ganhador player1')
-}else{
-    alert('ganhador player2')
-}
 
 
 
