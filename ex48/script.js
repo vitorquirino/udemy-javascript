00155b074c77c72c348a1c4ecb1242fa95dd381a
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-07-08 13:18:23 -0300

    relembrando

--- ex48/script.js
+++ ex48/script.js
@@ -1,7 +1,15 @@
-// switch
-//case
+var n1 = prompt('digite um numero');
+n1 = parseFloat(n1);
 
-
-
-
+for(let i = 1; i <= 1000; i++){
+    document.write(n1 + 'x' + i + '=' + (n1 * i) + '<br>')
+}
+//var indicie = 1001;
+//do{
+  //  document.write(n1 + 'x' + indicie + '=' + (n1 * indicie) + '<br>')
+    //indicie = indicie + 1;
+    //indicie += 1 ;
+  //  indicie++;
+//} while(indici <= 1000)
+//document.write('saiu do loop')
 
