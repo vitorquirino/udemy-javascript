7e5d0e12f69f9f10c80487ee3fced38aac73c6db
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-07-11 15:22:13 -0300

    Update script.js

--- ex49/script.js
+++ ex49/script.js
@@ -1,18 +1,5 @@
-function soma (x,y){
-    if(
-        typeof x !== 'number' || 
-        typeof y !== 'number'
-    ){
-        throw new TypeError('x e y precisam ser numeros');
-    }
-    return x+y;
-}
 
-try{
-    console.log(soma(1,2));
-    console.log(soma('1',2));
-} catch(err){
-    console.log(err)
-    console.log("alguma coisa mais amigável pro usuário")
-}
+
+
+
 
