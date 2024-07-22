af9c35fa97514b7e85221eb9fd6ca596a278ea2d
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-07-22 14:24:16 -0300

    .

--- ex48/script.js
+++ ex48/script.js
@@ -1,1 +1,10 @@
-var output = document.querySelector('#output');
+var btn = document.querySelector('#refresh');
+btn.addEventListener('click', () => {
+    location.reload()
+});
+
+var numeroDigitado = prompt('digite um numero');
+numeroDigitado = parseFloat(numeroDigitado);
+for (var i = 1; i <= 1000; i++ ){t
+    document.write(numeroDigitado + ' x ' + i + ' = ' + (numeroDigiado * i) + '<br>')
+}
