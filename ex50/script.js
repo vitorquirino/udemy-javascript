d060a9d48defac4a0be7c140664682d06a0777b1
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-08-05 14:42:49 -0300

    exercicio!

--- ex50/script.js
+++ ex50/script.js
@@ -1,4 +1,35 @@
-function mostrarTabuada(){
-    
+var n1 = prompt('digite um numero');
+n1 = parseFloat(n1);
+
+escreve();
+
+function escreve (){
+    var output= document.querySelector('#output');
+    output.innerHTML = n1;
 };
+
+function somar (){
+    var n = prompt('digite outro numero');
+    n = parseFloat(n);
+    n1 = n1 + n;
+    escreve();
+}
+function subtrair (){
+    var n = prompt('digite outro numero');
+    n = parseFloat(n);
+    n1 = n1 - n;
+    escreve();
+}
+function multiplicar (){
+    var n = prompt('digite outro numero');
+    n = parseFloat(n);
+    n1 = n1 * n;
+    escreve();
+}
+function dividir (){
+    var n = prompt('digite outro numero');
+    n = parseFloat(n);
+    n1 = n1 / n;
+    escreve();
+}
 
