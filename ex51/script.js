24700135ae4f2b1759691556d1a122d9252fdbd3
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2024-08-12 14:43:37 -0300

    ad classe

---
+++ ex51/script.js
@@ -0,0 +1,6 @@
+var largura = parseFloat(prompt('digite a largura'));
+var altura = parseFloat(prompt('digite a altura :'));
+
+function mostrarArea (l , a){
+    alert('Área total : ' + (l * a ));
+}
