e57ddc76faafa0fa71720a7ede582cca61f8f5da
Quirino <vquirino671@gmail.com>
2024-09-09 11:51:25 -0300

    add

--- ex52/index.js
+++ ex52/index.js
@@ -1,4 +1,1 @@
-const jantar = Number(prompt('valor da janta'));
-const garcom = jantar * 0.10;
-const total = jantar + garcom;
-alert("taxa garcom R$ " + garcom.toFixed(2)  + " \ntotal " + total.toFixed(2));
+
---
+++ ex53/livro.js
@@ -0,0 +1,5 @@
+const dia = Number(prompt('digite numero de dias : '));
+const horas = Number(prompt('digite numero de horas : '));
+const result = (dia * 24 ) + horas;
+alert('total de horas : ' + result);
+
