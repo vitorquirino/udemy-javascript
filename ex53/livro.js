5f9fd901e6ffc8af4dc0772713787d898358bcd3
vitorquirino <vquirino671@gmail.com>
2024-09-16 11:54:29 -0300

    resolvido outro exercicio do livro

--- ex53/livro.js
+++ ex53/livro.js
@@ -1,7 +1,20 @@
-const casa = Number(prompt("numero da casa"));
-const vizinho1 = (casa + 1);
-const vizinho2 = (casa - 1);
-alert("numero da sua casa: "+ casa);
-alert("Vizinhos : " + vizinho2 + " e " + vizinho1)
+//const casa = Number(prompt("numero da casa"));
+//const vizinho1 = (casa + 1);
+//const vizinho2 = (casa - 1);
+//alert("numero da sua casa: "+ casa);
+//alert("Vizinhos : " + vizinho2 + " e " + vizinho1)
+
+//const valorFinal = 90.00;
+//const numeroClientes = 3;
+//const valorPcliente = (valorFinal / numeroClientes);
+//console.log('valor da conta: ' + valorFinal + '\n' +'numero de clientes: ' + numeroClientes + '\n' +
+ //'valor por cliente: ' + valorPcliente) + '\n';
 
 
+
+
+const valorFinal = prompt('digite o valor final da conta :');
+const numeroClientes = prompt('quantos clientes tem :');
+const valorPcliente = (valorFinal / numeroClientes);
+alert('valor da conta: ' + valorFinal + '\n' +'numero de clientes: ' + numeroClientes + '\n' +
+'valor por cliente: ' + valorPcliente) + '\n';
